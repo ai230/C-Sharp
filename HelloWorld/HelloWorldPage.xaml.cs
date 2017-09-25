488137acaa030713371fff63c51b00607e0690ae
AiYamamoto <ay36230@gmail.com>
2017-09-25 15:31:16 -0700

    Created Helloworld

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+            slider1.Value = 0.5;
+            if(Device.RuntimePlatform.Equals("iOS"))
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            }
+            else if(Device.RuntimePlatform.Equals("Android"))
+            {
+                Padding = new Thickness(20, 0, 0, 0);
+            }
+            else if(Device.RuntimePlatform.Equals("Windows"))
+            {
+                Padding = new Thickness(0, 0, 20, 0);
+            }
+        }
+    }
+}
